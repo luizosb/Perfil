66dfd9c721271498ae6d31ece97bd0d7ca0ab8c6
luizosb <56880799+luizosb@users.noreply.github.com>
2020-01-22 21:04:28 -0300

    Perfil So home
    ate agr so tem home

---
+++ perfil/src/test/java/com/perfil/PerfilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
